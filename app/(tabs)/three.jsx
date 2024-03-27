f7243d90204c83e9f0ba70df8ed1da01182d1018
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-03-27 18:26:33 +0530

    tab navigation

---
+++ app/(tabs)/three.jsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function TabThreeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab Three</Text>
+      <View style={styles.separator}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
--- app/+not-found.jsx
+++ app/+not-found.jsx
@@ -1,7 +1,7 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { View } from 'react-native-web';
 
-import { Text, View } from '@/components/Themed';
 
 export default function NotFoundScreen() {
   return (
